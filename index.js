d85acdefa662ce25b294d275138adc6eea14baa2
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-04-05 09:42:38 +0200

    Calculator el js no funciona y no tengo ni idea de porque no va sorry😊

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+function  clickNumber(btn){
+
+    console.log(btn.value);
+}
+
+const $numbers = document.getElementsByClassName("number")
+console.log($numbers.item(1));
+
+function click(){
+    console.log("ASdas");
+}
