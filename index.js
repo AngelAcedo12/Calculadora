93be5a832e3720e1a5f86937817297e98f51c4da
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-04-05 10:50:58 +0200

    click in number

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-
-
-
-
-
-function  clickNumber(btn){
-
-    console.log(btn.value);
-}
-
+// Estas dos lineas si funcionan apartir de ellas no funciona nada
 const $numbers = document.getElementsByClassName("number")
-console.log($numbers.item(1));
+const $input = document.querySelector("input")
+const $rest = document.getElementsByClassName("rest").item(0)
 
-function click(){
-    console.log("ASdas");
+for (let index = 0; index < $numbers.length; index++) {
+    $numbers.item(index).addEventListener("click", () => {
+        $input.value=$input.value.concat($numbers.item(index).innerHTML);
+    })
 }
+
+$rest.addEventListener("click" , ()=>{
+ 
+    if((!$input.value.includes("-") && $input.value.startsWith("")) || (($input.value.includes("-")))){
+        $input.value=$input.value.concat("-")
+    }
+})
+
+
+
