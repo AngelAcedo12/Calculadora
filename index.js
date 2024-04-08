5a456d7f78c8820b2fa0e56cb50fa209ba634ed2
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-04-08 08:53:25 +0200

    calcular operationes complete

--- index.js
+++ index.js
@@ -2,12 +2,31 @@
 const $numbers = document.getElementsByClassName("number")
 const $input = document.querySelector("input")
 const $rest = document.getElementsByClassName("rest").item(0)
+const $clear = document.getElementsByClassName("clear").item(0)
+const $igual = document.getElementsByClassName("igual").item(0)
+const $removed_one = document.getElementsByClassName("remov-one").item(0)
+const $sum = document.getElementsByClassName("sum").item(0)
+const $mult = document.getElementsByClassName("mult").item(0)
+const $div = document.getElementsByClassName("dividir").item(0) 
+
+
+$sum.addEventListener("click", ()=> {
+    
+   if(operacion.operacion==null && operacion.firstNumber){
+   }
+})
+
+$clear.addEventListener("click",()=>{
+    $input.value=""
+})
+
 
 for (let index = 0; index < $numbers.length; index++) {
     $numbers.item(index).addEventListener("click", () => {
         $input.value=$input.value.concat($numbers.item(index).innerHTML);
     })
 }
+
 
 $rest.addEventListener("click" , ()=>{
  
@@ -16,5 +35,35 @@ $rest.addEventListener("click" , ()=>{
     }
 })
 
+$removed_one.addEventListener("click",()=>{
+    $input.value=$input.value.substring(0,$input.value.length-1)
+})
+
+$mult.addEventListener("click", () => {
+    if((!$input.value.includes("*") && $input.value.startsWith("")) || (($input.value.includes("*")))){
+        $input.value=$input.value.concat("*")
+    }
+})
+
+$div.addEventListener("click", () => {
+    if((!$input.value.includes("/") && $input.value.startsWith("")) || (($input.value.includes("/")))){
+        $input.value=$input.value.concat("/")
+    }
+})
+
+
+$igual.addEventListener("click",()=>{
+    $input.value=eval($input.value)
+
+})
+
+
+function savedInLocalStorage(){
+    
+}
+
+function getOperationInLocalStorage(){
+
+}
 
 
